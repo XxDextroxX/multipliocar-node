4f6967d4416ed6028636098219478e28bdac842d
XxDextroxX <jesnaruto37@gmail.com>
2020-08-19 11:24:44 -0500

    'Commi'

---
+++ config/yargs.js
@@ -0,0 +1,20 @@
+const opt = {
+    base: {
+        demand: true,
+        alias: 'b'
+    },
+    limite: {
+        alias: 'l',
+        default: 10
+    },
+}
+
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola', opt)
+    .command('crear', 'crear tabla', opt)
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
